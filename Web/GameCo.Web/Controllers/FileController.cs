75c86f05034c26bd2ab1cff07f5b345de3e8ba32
George <44335333+Georgi1102@users.noreply.github.com>
2021-01-31 18:44:50 +0200

    SeventhCommit/GoPo
    U can upload files

---
+++ Web/GameCo.Web/Controllers/FileController.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GameCo.Web.Controllers
+{
+    
+    public class FileController : Controller
+    {
+        private const string folderToUpload = "UploadedFiles";
+
+        private string filePath;
+        private bool isUploaded;
+        
+
+        [HttpGet]
+        public IActionResult FileUpload()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> FileUpload(IFormFile someFile)
+        {
+            await Upload(someFile);
+            TempData["msg"] = "The selected file was successfully uploaded";
+            return View();
+        }
+
+        public async Task<bool> Upload(IFormFile someFile)
+        {
+            filePath = "";
+            isUploaded = false;
+    
+            try
+            {
+                if (someFile.Length > 0)
+                {
+                    string fileName = Path.GetFileName(someFile.FileName);
+                    filePath = Path.GetFullPath(Path.Combine(Directory.GetCurrentDirectory(), folderToUpload));
+
+                    
+                    //DO NOT FORGET TO CLOSE THE STREAM!!!
+                    using (var fileStream = new FileStream(Path.Combine(filePath, fileName), FileMode.Create))
+                    {
+                        await someFile.CopyToAsync(fileStream);
+                        fileStream.Close();
+                    }
+                    isUploaded = true;
+                }
+
+                else
+                {
+                    isUploaded = false;
+                }
+            }
+            catch (Exception)
+            {
+
+                throw new FileNotFoundException();
+            }
+
+            return isUploaded;
+        }
+    }
+}
---
+++ Web/GameCo.Web/wwwroot/js/Redirect.js
@@ -0,0 +1,20 @@
+﻿let counter = document.querySelector('h1');
+let count = 1;
+var span = document.getElementById('currState');
+var text = span.textContent;
+
+if (text == "The selected file was successfully uploaded") {
+
+    setInterval(() => {
+        count++;
+        counter.innerText = "Can proceed now";
+        if (count == 5) {
+            window.location.replace("https://localhost:44353");
+        }
+
+    },1000)
+}
+
+else {
+    counter.innerText = "Can not proceed";
+}
