88f30eefdaa0ead67796f6d3d1ae6ad231e95ef1
Herion959 <68737714+Herion959@users.noreply.github.com>
2021-02-05 21:35:03 +0200

    TwelvethCommit

--- Web/GameCo.Web/Program.cs
+++ Web/GameCo.Web/Program.cs
@@ -15,7 +15,6 @@ namespace GameCo.Web
         {
             CreateHostBuilder(args).Build().Run();
         }
-
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
